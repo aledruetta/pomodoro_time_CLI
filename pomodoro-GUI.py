aff738e845aad9f754c1ef99f0b39ee4ecb4aeb3
Alejandro Druetta <aledruetta@gmail.com>
2016-03-07 17:02:05 -0300

    Entry & colors

--- pomodoro-GUI.py
+++ pomodoro-GUI.py
@@ -10,7 +10,8 @@
 # Usage:
 
 import tkinter as tk
-from tkinter import Tk, Frame, Label, Button, StringVar
+from tkinter import simpledialog
+from tkinter import Tk, Frame, Label, Button, Entry, StringVar
 import sqlite3 as sql
 import sys
 from os import path
@@ -20,20 +21,25 @@ from time import time, gmtime, strftime, sleep
 T_WORK = 0.5
 T_BREAK = T_WORK * 0.2
 T_LONG = T_WORK * 0.6
+YELLOW = "#fcf3cf"
+BLUE = "#2874a6"
+RED = "#e74c3c"
+GREEN = "#229954"
+ORANGE = "#dc7633"
 
 
 class Pomodoro(Frame):
 
     def __init__(self, parent, abspath):
         super().__init__(parent)
-        self.top = parent
+        self.master = parent
         self.abspath = abspath
 
         self.work_count = 0
 
         # Tk variables
         self.tagVar = StringVar()
-        self.tagVar.set("unknown")
+        self.tagVar.set("")
         self.actionVar = StringVar()
         self.actionVar.set("Work")
         self.displayVar = StringVar()
@@ -43,58 +49,60 @@ class Pomodoro(Frame):
 
     def initUI(self):
         # toplevel
-        self.top.title("PomodoroPy")
-        self.top.resizable(0, 0)
+        self.master.title("PomodoroPy")
+        self.master.resizable(0, 0)
 
         # main frame
-        self["relief"] = "solid"
         self.pack()
 
+        self.entryTag = Entry(self, textvariable=self.tagVar)
+        self.entryTag["font"] = "helvetica 14 bold"
+        self.entryTag.bind('<Return>', self.catchTag)
+        self.entryTag.pack(expand=True, fill=tk.X)
+
         self.timeLabel = Label(self, textvariable=self.displayVar)
+        self.timeLabel["background"] = YELLOW
         self.timeLabel["padx"] = "10px"
         self.timeLabel["font"] = "helvetica 48 bold"
-        self.timeLabel["relief"] = "raised"
         self.timeLabel["fg"] = "gray"
         self.timeLabel.pack(expand=True, fill=tk.X)
 
-        self.buttonFrame = Frame(self)
-        self.buttonFrame.pack(expand=True, fill=tk.X)
-
-        self.actionButton = Button(self.buttonFrame)
+        self.actionButton = Button(self)
         self.actionButton["text"] = "Work"
         self.actionButton["font"] = "helvetica 16"
         self.actionButton["command"] = lambda: self.action(
             self.actionButton.cget("text"))
         self.actionButton.pack(expand=True, fill=tk.X)
 
-        self.statusLabel = Label(self, textvariable=self.tagVar)
-        self.statusLabel["relief"] = "ridge"
-        self.statusLabel.pack(expand=True, anchor=tk.NW)
+    def catchTag(self, event=None):
+        self.entryTag["state"] = "readonly"
+        self.tagVar.set(self.tagVar.get().upper())
 
     def action(self, action):
         if action == "Work":
             self.work_count += 1
-            self.timeLabel["fg"] = "blue"
+            self.timeLabel["fg"] = BLUE
             self.actionButton["text"] = "Pause"
             self.clock(T_WORK)
             self.actionButton["text"] = "Break"
         elif action == "Pause":
-            self.timeLabel["fg"] = "red"
+            self.timeLabel["fg"] = RED
             self.actionButton["text"] = "Continue"
         elif action == "Continue":
-            self.timeLabel["fg"] = "blue"
+            self.timeLabel["fg"] = BLUE
             self.actionButton["text"] = "Pause"
         elif action == "Break":
-            self.actionButton["state"] = "disabled"
+            self.actionButton["state"] = "disable"
             if self.work_count < 4:
-                self.timeLabel["fg"] = "green"
+                self.timeLabel["fg"] = GREEN
                 self.clock(T_BREAK)
             elif self.work_count >= 4:
-                self.timeLabel["fg"] = "orange"
+                self.timeLabel["fg"] = ORANGE
                 self.clock(T_LONG)
                 self.work_count = 0
-            self.actionButton["state"] = "normal"
+            self.entryTag["state"] = "normal"
             self.actionButton["text"] = "Work"
+            self.actionButton["state"] = "normal"
 
     def clock(self, minutes):
         finish = time() + minutes * 60
@@ -113,9 +121,9 @@ class Pomodoro(Frame):
 def main():
     dirname = path.dirname(sys.argv[0])
     abspath = path.abspath(dirname)
-    top = Tk()
-    app = Pomodoro(top, abspath)
-    top.mainloop()
+    master = Tk()
+    app = Pomodoro(master, abspath)
+    master.mainloop()
 
 
 if __name__ == '__main__':
