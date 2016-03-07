66162cc833e8933fdb4e7a66a4b6c6091e87bb42
Alejandro Druetta <aledruetta@gmail.com>
2016-03-07 14:13:00 -0300

    work, pause, continue, break

--- pomodoro-GUI.py
+++ pomodoro-GUI.py
@@ -16,15 +16,29 @@ import sys
 from os import path
 from time import time, gmtime, strftime, sleep
 
+# Constants
+T_WORK = 0.5
+T_BREAK = T_WORK * 0.2
+T_LONG = T_WORK * 0.6
+
 
 class Pomodoro(Frame):
+
     def __init__(self, parent, abspath):
         super().__init__(parent)
         self.top = parent
         self.abspath = abspath
-        self.t_work = 1
-        self.t_break = self.t_work * 0.2
-        self.t_long = self.t_work * 0.6
+
+        self.work_count = 0
+
+        # Tk variables
+        self.tagVar = StringVar()
+        self.tagVar.set("unknown")
+        self.actionVar = StringVar()
+        self.actionVar.set("Work")
+        self.displayVar = StringVar()
+        self.displayVar.set("00:00")
+
         self.initUI()
 
     def initUI(self):
@@ -36,10 +50,7 @@ class Pomodoro(Frame):
         self["relief"] = "solid"
         self.pack()
 
-        self.display_time = StringVar()
-        self.display_time.set("00:00")
-
-        self.timeLabel = Label(self, textvariable=self.display_time)
+        self.timeLabel = Label(self, textvariable=self.displayVar)
         self.timeLabel["padx"] = "10px"
         self.timeLabel["font"] = "helvetica 48 bold"
         self.timeLabel["relief"] = "raised"
@@ -49,50 +60,54 @@ class Pomodoro(Frame):
         self.buttonFrame = Frame(self)
         self.buttonFrame.pack(expand=True, fill=tk.X)
 
-        self.workButton = Button(self.buttonFrame)
-        self.workButton["command"] = lambda: self.clock(self.t_work)
-        self.workButton["text"] = "Work"
-        self.workButton["font"] = "helvetica 16"
-        self.workButton.pack(expand=True, fill=tk.X)
+        self.actionButton = Button(self.buttonFrame)
+        self.actionButton["text"] = "Work"
+        self.actionButton["font"] = "helvetica 16"
+        self.actionButton["command"] = lambda: self.action(
+            self.actionButton.cget("text"))
+        self.actionButton.pack(expand=True, fill=tk.X)
 
-        self.breakButton = Button(self.buttonFrame)
-        self.breakButton["text"] = "Break"
-        self.breakButton["font"] = "helvetica 16"
-        self.breakButton.pack(expand=True, fill=tk.X)
-
-        self.stopButton = Button(self.buttonFrame)
-        self.stopButton["text"] = "Stop"
-        self.stopButton["font"] = "helvetica 16"
-        self.stopButton.pack(expand=True, fill=tk.X)
-
-        self.tag = StringVar()
-        self.tag.set("unknown")
-
-        self.statusLabel = Label(self, textvariable=self.tag)
+        self.statusLabel = Label(self, textvariable=self.tagVar)
         self.statusLabel["relief"] = "ridge"
         self.statusLabel.pack(expand=True, anchor=tk.NW)
 
-    def main_loop(self):
-        self.break_count = IntegerVar()
-        self.break_count.set(0)
+    def action(self, action):
+        if action == "Work":
+            self.work_count += 1
+            self.timeLabel["fg"] = "blue"
+            self.actionButton["text"] = "Pause"
+            self.clock(T_WORK)
+            self.actionButton["text"] = "Break"
+        elif action == "Pause":
+            self.timeLabel["fg"] = "red"
+            self.actionButton["text"] = "Continue"
+        elif action == "Continue":
+            self.timeLabel["fg"] = "blue"
+            self.actionButton["text"] = "Pause"
+        elif action == "Break":
+            self.actionButton["state"] = "disabled"
+            if self.work_count < 4:
+                self.timeLabel["fg"] = "green"
+                self.clock(T_BREAK)
+            elif self.work_count >= 4:
+                self.timeLabel["fg"] = "orange"
+                self.clock(T_LONG)
+                self.work_count = 0
+            self.actionButton["state"] = "normal"
+            self.actionButton["text"] = "Work"
 
     def clock(self, minutes):
-        if minutes == self.t_work:
-            color = "blue"
-        elif minutes == self.t_break:
-            color = "green"
-        elif minutes == self.t_long:
-            color = "orange"
-
-        self.timeLabel["fg"] = color
-
         finish = time() + minutes * 60
         while(time() < finish):
-            seconds = finish - time()
-            remaining = gmtime(seconds)
-            self.display_time.set(strftime("%M:%S", remaining))
-            sleep(1)
-            self.timeLabel.update()
+            self.actionButton.update()
+            if self.actionButton.cget("text") != "Continue":
+                seconds = finish - time()
+                remaining = gmtime(seconds)
+                self.displayVar.set(strftime("%M:%S", remaining))
+                self.update_idletasks()
+                sleep(1)
+            else:
+                finish = time() + seconds
 
 
 def main():
